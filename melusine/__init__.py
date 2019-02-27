f009198eeb50c3bbb7759b365758da03d88174c8
Tom Stringer <tstringer@quantmetry.com>
2019-02-27 18:35:18 +0100

    Bump version: 1.3.0 → 1.4.0

--- melusine/__init__.py
+++ melusine/__init__.py
@@ -2,4 +2,4 @@
 
 __author__ = """Sacha Samama & Tom Stringer"""
 __email__ = 'ssamama@quantmetry.com, tstringer@quantmetry.com'
-__version__ = '1.3.0'
+__version__ = '1.4.0'
--- setup.py
+++ setup.py
@@ -64,6 +64,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MAIF/melusine',
-    version='1.3.0',
+    version='1.4.0',
     zip_safe=False,
 )
