e370925ea76c654dd4b90918f63dc76f87322c29
Tom Stringer <tstringer@quantmetry.com>
2019-02-27 18:24:24 +0100

    Bump version: 1.2.1 → 1.3.0

--- setup.py
+++ setup.py
@@ -64,6 +64,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MAIF/melusine',
-    version='1.2.1',
+    version='1.3.0',
     zip_safe=False,
 )
