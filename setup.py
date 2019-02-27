1d9e73475957a9374e1a0a984b32e5c8919f85ab
sachasamama <ssamama@quantmetry.com>
2019-02-27 18:11:11 +0100

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -64,6 +64,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MAIF/melusine',
-    version='1.0.2',
+    version='1.0.3',
     zip_safe=False,
 )
