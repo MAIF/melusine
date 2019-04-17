2806df5569d55d331d4186fb9cfaf97c98cfb092
LEBRETON Benoit <benoit.lebreton@externe.maif.fr>
2019-04-17 15:04:03 +0200

    progress_bar using tqdm
    the progress_bar is updated simultanously by every worker

--- melusine/utils/multiprocessing.py
+++ melusine/utils/multiprocessing.py
@@ -26,6 +26,22 @@ def apply_by_multiprocessing(df, func, n_jobs=1, **kwargs):
         Returns the DataFrame with the function applied.
     """
     result = joblib.Parallel(n_jobs=n_jobs, prefer='processes')(
-        joblib.delayed(d.apply)(func, **kwargs) for d in np.array_split(df, n_jobs)
+        joblib.delayed(apply_on_chunk)(d, func, **kwargs) for d in np.array_split(df, n_jobs)
     )
     return pd.concat(list(result))
+
+def apply_on_chunk(X, func, **kwargs):
+    if "progress_bar" in kwargs:
+        progress_bar = kwargs.pop('progress_bar')
+    else:
+        progress_bar = False
+    X = apply_df(X, func, progress_bar)
+    return X
+
+def apply_df(df, func, progress_bar):
+    if progress_bar:
+        tqdm.pandas(leave=False, desc=func.__name__, unit='emails', dynamic_ncols=True, mininterval=2.0)
+        df = df.progress_apply(func, axis=1)
+    else:
+        df = df.apply(func, axis=1)
+    return df
