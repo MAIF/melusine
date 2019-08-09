e694221bbafb64ebf49eb7c8ed438ad13f832835
Marouane IL IDRISSI <marouane.il.idrissi@maif.fr>
2019-08-09 10:54:45 +0200

    Adding new embeddings

--- melusine/nlp_tools/embedding.py
+++ melusine/nlp_tools/embedding.py
@@ -1,5 +1,11 @@
 import logging
 from gensim.models import Word2Vec
+from gensim.models.keyedvectors import Vocab, KeyedVectors
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer
+# from glove import Corpus, Glove
+
 from melusine.utils.streamer import Streamer
 
 log = logging.getLogger('Embeddings')
@@ -8,19 +14,15 @@ formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(messag
                               datefmt='%d/%m %I:%M')
 
 
-class Embedding():
+class Embedding:
     """Class to train embeddings with Word2Vec algorithm.
-
     Attributes
     ----------
     input_column : str,
         Input text column to consider for the embedding.
-
     stream : Streamer instance,
         Builds a stream a tokens from a pd.Dataframe to train the embeddings.
-
     embedding : Word2Vec instance from Gensim
-
     Examples
     --------
     >>> from melusine.nlp_tools.embedding import Embedding
@@ -28,7 +30,6 @@ class Embedding():
     >>> embedding.train(X)
     >>> embedding.save(filepath)
     >>> embedding = Embedding().load(filepath)
-
     """
 
     def __init__(self,
@@ -40,6 +41,7 @@ class Embedding():
                  window=5,
                  min_count=100,
                  stop_removal = True):
+
         self.logger = logging.getLogger('NLUtils.Embedding')
         ch = logging.StreamHandler()
         ch.setLevel(logging.INFO)
@@ -55,6 +57,9 @@ class Embedding():
         self.size = size
         self.window = window
         self.min_count = min_count
+        self.word2id = {}
+        self.embedding = None
+        self.kv = None
 
     def save(self, filepath):
         """Method to save Embedding object."""
@@ -62,34 +67,161 @@ class Embedding():
 
     def load(self, filepath):
         """Method to load Embedding object."""
-        self.embedding = Word2Vec.load(filepath)
+        self.embedding = KeyedVectors.load(filepath, mmap='r')
         return self
 
-    def train(self, X):
-        """Train embeddings with Word2Vec algorithm.
-
+    def train(self, X, embedding_type='word2vec'):
+        """Train embeddings with the desired word embedding algorithm (default is Word2Vec).
         Parameters
         ----------
         X : pd.Dataframe
             Containing a clean body column.
-
+        embedding_type: str
+            Desired embedding type
         Returns
         -------
         self : object
             Returns the instance
         """
         self.logger.info('Start training for embedding')
+
         self.streamer.to_stream(X)
-        self.embedding = Word2Vec(workers=self.workers,
-                                  seed=self.seed,
-                                  iter=self.iter,
-                                  size=self.size,
-                                  window=self.window,
-                                  min_count=self.min_count)
-        self.embedding.build_vocab(self.streamer.stream)
-        self.embedding.train(self.streamer.stream,
-                             total_examples=self.embedding.corpus_count,
-                             epochs=self.embedding.epochs)
+
+        if embedding_type == 'word2vec':
+            self.train_word2vec()
+        elif embedding_type == 'tfidf':
+            self.train_tfidf(X, self.input_column)
+        elif embedding_type == 'docterm':
+            self.train_docterm(X, self.input_column)
+        #elif embedding_type == 'glove':
+        #    self.train_glove(X, self.input_column)
 
         self.logger.info('Done.')
-        pass
+
+    def train_tfidf(self, X, input_column):
+        """TODO
+        Parameters
+        ----------
+        X : pd.Dataframe
+            Containing a column with tokens.
+        input_column: str
+            Name of the input column containing tokens
+        """
+
+        def dummy_function(doc):
+            return doc
+
+        tfidf_vec = TfidfVectorizer(
+            analyzer='word',
+            tokenizer=dummy_function,
+            preprocessor=dummy_function,
+            token_pattern=None,
+            use_idf=True,
+            norm='l2'
+        )
+
+        tfidf_data = tfidf_vec.fit_transform(X[input_column])
+
+        self.word2id = tfidf_vec.vocabulary_
+        embedding_matrix = self.train_svd(tfidf_data)
+
+        self.create_keyedvector_from_matrix(embedding_matrix, self.word2id)
+
+    def train_docterm(self, X, input_column):
+        """TODO
+        Parameters
+        ----------
+        X : pd.Dataframe
+            Containing a column with tokens.
+        input_column: str
+            Name of the input column containing tokens
+        """
+
+        def dummy_function(doc):
+            return doc
+
+        # CountVectorizer
+
+        count_vec = CountVectorizer(
+            analyzer='word',
+            tokenizer=dummy_function,
+            preprocessor=dummy_function,
+            token_pattern=None
+        )
+
+        docterm_data = count_vec.fit_transform(X[input_column])
+
+        self.word2id = count_vec.vocabulary_
+        embedding_matrix = self.train_svd(docterm_data)
+
+        self.create_keyedvector_from_matrix(embedding_matrix, self.word2id)
+
+    def train_svd(self, vectorized_corpus_data):
+        """TODO
+        Parameters
+        ----------
+        vectorized_corpus_data: TODO
+            Vectorized data TODO
+        """
+
+        svd=TruncatedSVD(n_components=self.size)
+        svd.fit(vectorized_corpus_data)
+
+        embedding_matrix = svd.components_.T
+        return embedding_matrix
+
+    def train_word2vec(self):
+        """TODO
+        Parameters
+        ----------
+        """
+
+        embedding = Word2Vec(workers=self.workers,
+                             seed=self.seed,
+                             iter=self.iter,
+                             size=self.size,
+                             window=self.window,
+                             min_count=self.min_count)
+        embedding.build_vocab(self.streamer.stream)
+        embedding.train(self.streamer.stream,
+                        total_examples=embedding.corpus_count,
+                        epochs=embedding.epochs)
+
+        self.embedding = embedding.wv
+
+    #def train_glove(self, X, input_column):
+    #
+    #    corpus = Corpus()
+    #
+    #    corpus.fit(X[input_column], window=self.window)
+    #
+    #    self.word2id = corpus.dictionary
+    #
+    #    glove=Glove(no_components=self.size)
+    #    glove.fit(corpus.matrix, epochs=self.iter, no_threads=self.workers, )
+    #
+    #    self.create_keyedvector_from_matrix(glove.word_vectors, self.word2id)
+
+    def create_keyedvector_from_matrix(self, embedding_matrix, word2id):
+        """TODO
+        Parameters
+        ----------
+        embedding_matrix: numpy.ndarray
+            Embedding matrix as a numpy object
+        word2id: dict
+            Word vocabulary (key: word, value: word_index)
+        """
+
+        vocab = word2id
+        embedding_matrix = embedding_matrix
+        vector_size = embedding_matrix.shape[1]
+
+        kv = KeyedVectors(vector_size)
+        kv.vector_size = vector_size
+        kv.vectors = embedding_matrix
+
+        kv.index2word = list(vocab.keys())
+
+        kv.vocab = {word: Vocab(index=word_id, count=0) for word, word_id in vocab.items()}
+
+        self.embedding = kv
