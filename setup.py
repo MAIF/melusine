023cac29a636063233e2e911858305b8f57efe53
Hugo Perrier <hperrier@quantmetry.com>
2019-09-23 16:05:20 +0200

    Bump version: 1.8.2 → 1.8.3

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MAIF/melusine',
-    version='1.8.2',
+    version='1.8.3',
     zip_safe=False,
 )
