ffd81dbb7c544208e74bc3d4317f1ad49363f40b
Benoit Lebreton <blebreton@quantmetry.com>
2019-09-28 10:56:48 +0200

    modify the test to show the improvement of the precedent commit

--- tests/unit_tests/prepare_email/test_build_historic.py
+++ tests/unit_tests/prepare_email/test_build_historic.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from melusine.prepare_email.build_historic import build_historic
 
-body = " \n  \n  \n Bonjours, \n  \n Suite a notre conversation téléphonique \
+body = "En date de lun. 10 mai 2018 LeSociétaire a écrit\nObjet : Demande de régularisation \n  \n  \n Bonjours, \n  \n Suite a notre conversation téléphonique \
 de Mardi , pourriez vous me dire la \n somme que je vous dois afin d'd'être \
 en régularisation . \n  \n Merci bonne journée \n  \n Le mar. 22 mai 2018 \
 à 10:20,  <gestionsocietaire@mutuelle.fr> a écrit\xa0: \n\nObjet : Envoi d'un document \n\n Bonjour. \n  \n \
@@ -12,10 +12,10 @@ nécessite Adobe Reader. \n  "
 
 
 output = [
-    {'text': " \n  \n  \n Bonjours, \n  \n Suite a notre conversation \
+    {'text': "Bonjours, \n  \n Suite a notre conversation \
 téléphonique de Mardi , pourriez vous me dire la \n somme que je vous \
 dois afin d'd'être en régularisation . \n  \n Merci bonne journée",
-     'meta': ''},
+     'meta': 'En date de lun. 10 mai 2018 LeSociétaire a écrit\nObjet : Demande de régularisation \n  \n  \n '},
     {'text': "Bonjour. \n  \n Merci de bien vouloir prendre connaissance \
 du document ci-joint : \n 1 - Relevé d'identité postal MUTUELLE \
 (contrats) \n  \n Sentiments mutualistes. \n  \n La Mututelle \n  \n \
