070d8e1a1bf97e2992bbdcba445c6916bae8e908
Hugo Perrier <hperrier@quantmetry.com>
2019-11-27 10:08:39 +0100

    melusine version

--- setup.py
+++ setup.py
@@ -67,6 +67,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MAIF/melusine',
-    version='1.8.7',
+    version='1.8.6',
     zip_safe=False,
 )
