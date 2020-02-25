1912dfac4441e38c1d48adb423755a8edf8cc03c
Florian <farthur@quantmetry.com>
2020-02-25 16:41:32 +0100

    Fix test since there were based on malformated regex

--- tests/unit_tests/prepare_email/test_cleaning.py
+++ tests/unit_tests/prepare_email/test_cleaning.py
@@ -43,12 +43,12 @@ def test_remove_transfer_answer_header(input_str, expected_str):
     ("01 23 45 67 89 et 01.23.45.67.89", " flag_phone_  et  flag_phone_ "),
     ("mon numéro01 23 45 67 89", "mon numéro flag_phone_ "),
     ("le montant du contrat est du 18000$, soit 17000euros",
-     "le montant du contrat est du flag_amount_ , soit flag_amount_ "),
+     "le montant du contrat est du  flag_amount_ , soit  flag_amount_ "),
     ("J'habite au 1 rue de la paix, Paris 75002",
-     "J'habite au 1 rue de la paix, Paris flag_cp_ "),
+     "J'habite au 1 rue de la paix, Paris  flag_cp_ "),
     ("Rendez-vous le 18 décembre 2019 ou le 19/12/19 ou le 20.12.19 à 14h30",
-     "Rendez-vous le  flag_date_  ou le  flag_date_ ou le  flag_date_ à  flag_time_ "),
-    ("le 14/12 tu me devras 20.05 dollars", "le  flag_date_ tu me devras flag_amount_ ")
+     "Rendez-vous le  flag_date_  ou le  flag_date_  ou le  flag_date_  à  flag_time_ "),
+    ("le 14/12 tu me devras 20.05 dollars", "le  flag_date_  tu me devras  flag_amount_ ")
 ])
 def test_flag_items(input_str, expected_str):
     result = flag_items(input_str)
