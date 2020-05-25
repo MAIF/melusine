0aca68d17c8228b4fa1ed057b5237c6a3ae94958
Nicolas <npeltre@quantmetry.com>
2020-05-25 13:11:01 +0200

    Flake8 modifications

--- tests/unit_tests/nlp_tools/test_tokenizer.py
+++ tests/unit_tests/nlp_tools/test_tokenizer.py
@@ -5,11 +5,13 @@ from melusine.nlp_tools.tokenizer import Tokenizer
 
 @pytest.mark.parametrize("input_df, expected_serie", [
     (pd.DataFrame(["Bonjour, je m'appelle Nicolas : nicolas_nom@hotmail.fr"],
-                 columns=['body']),
-     pd.Series(["Bonjour", "appelle" , "flag_name_", "nicolas_nom", "hotmail","fr"])),
-    (pd.DataFrame(["Bonjour, je fais suite au devis réalisé pour le contrat en mai dernier. Cdlt, Jean-pierre."],
                   columns=['body']),
-     pd.Series(["Bonjour", "fais", "suite", "devis", "réalisé", "contrat", "mai", "dernier", "Cdlt", 'flag_name_']))
+     pd.Series(["Bonjour", "appelle", "flag_name_", "nicolas_nom", "hotmail", "fr"])),
+    (pd.DataFrame(["Bonjour, je fais suite au devis réalisé pour le contrat en mai dernier. "
+                   "Cdlt, Jean-pierre."],
+                  columns=['body']),
+     pd.Series(["Bonjour", "fais", "suite", "devis", "réalisé", "contrat", "mai", "dernier",
+                "Cdlt", 'flag_name_']))
 ])
 def test_flag_name(input_df, expected_serie):
     tokenizer = Tokenizer(input_column='body')
