b3a7be6019678d9b20eaf5cc4e5244b9747ca08c
Florian <farthur@quantmetry.com>
2020-06-22 10:17:56 +0200

    Bump version: 1.10.0 → 1.11.0

--- setup.py
+++ setup.py
@@ -72,6 +72,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/MAIF/melusine',
-    version='1.10.0',
+    version='1.11.0',
     zip_safe=False,
 )
