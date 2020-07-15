3673d6622748aae6aa59c9bdc9f758a3ce07a912
Tiph <tiphaine.fabre@maif.fr>
2020-07-15 16:03:18 +0200

    :package: pep8

--- tests/integration_test/test_classification.py
+++ tests/integration_test/test_classification.py
@@ -40,9 +40,9 @@ body = "Bonjour\nThis is Melusine\nCordialement\nDev Team"
 body = "Bonjour\nThis is Melusine\nCordialement\nDev Team"
 
 
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID";
-# The GPU id to use, usually either "0" or "1";
-os.environ["CUDA_VISIBLE_DEVICES"]="";
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
 
 def test_classification():
 
