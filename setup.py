4c44bebc1376c32d29c26c27e0b177496c7029c9
78169T <tiphaine.fabre@maif.fr>
2020-11-10 08:54:24 +0100

    Bump version: 2.2.3 → 2.2.4

--- setup.py
+++ setup.py
@@ -86,6 +86,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/MAIF/melusine",
-    version="2.2.3",
+    version="2.2.4",
     zip_safe=False,
 )
