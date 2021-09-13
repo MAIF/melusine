20aa605d117826df1ed885acdb141fdf2cc4b1f6
hperrier@quantmetry.com <B2obaXgit>
2021-09-13 15:36:07 +0200

    Create a Tokenizer class covering a larger scope.

--- melusine/nlp_tools/tokenizer.py
+++ melusine/nlp_tools/tokenizer.py
@@ -2,13 +2,20 @@ import logging
 import re
 import json
 from flashtext import KeywordProcessor
-from sklearn.base import BaseEstimator, TransformerMixin
 from melusine import config
+from typing import List, Dict
 
 logger = logging.getLogger(__name__)
 
 
-class WordLevelTokenizer(BaseEstimator, TransformerMixin):
+class WordLevelTokenizer:
+    """
+    Tokenizer which does the following:
+    - General flagging (using regex)
+    - Name flagging (using FlashText)
+    - Text splitting
+    - Stopwords removal
+    """
 
     # Attributes excluded from the default save methods
     # Ex: pickle.dump or json.dump
@@ -16,15 +23,35 @@ class WordLevelTokenizer(BaseEstimator, TransformerMixin):
 
     def __init__(
         self,
-        tokenizer_regex=config["tokenizer"]["tokenizer_regex"],
-        stopwords=config["tokenizer"]["stopwords"],
-        remove_stopwords=config["tokenizer"]["remove_stopwords"],
-        flag_dict=config["tokenizer"]["flag_dict"],
-        flashtext_separators=config["tokenizer"]["flashtext_separators"],
-        flashtext_names=config["tokenizer"]["flashtext_names"],
-        name_flag=config["tokenizer"]["name_flag"],
+        tokenizer_regex: str = config["tokenizer"]["tokenizer_regex"],
+        stopwords: List[str] = config["tokenizer"]["stopwords"],
+        remove_stopwords: bool = config["tokenizer"]["remove_stopwords"],
+        flag_dict: Dict[str, str] = config["tokenizer"]["flag_dict"],
+        flashtext_separators: List[str] = config["tokenizer"]["flashtext_separators"],
+        flashtext_names: List[str] = config["tokenizer"]["flashtext_names"],
+        name_flag: str = config["tokenizer"]["name_flag"],
         **kwargs
     ):
+        """
+
+        Parameters
+        ----------
+        tokenizer_regex: str
+            Regex used to split the text into tokkens
+        stopwords: List[str]
+            List of words to be removed
+        remove_stopwords: bool
+            If True, stopwords removal is enabled
+        flag_dict: Dict[str, str]
+            Flagging dict with regex as key and replace_text as value
+        flashtext_separators: List[Str]
+            List of separator words for FlashText
+        flashtext_names: List[Str]
+            List of names to be flagged
+        name_flag: str
+            Replace value for names
+        kwargs
+        """
 
         # Text splitting
         self.tokenizer_regex = tokenizer_regex
@@ -40,7 +67,10 @@ class WordLevelTokenizer(BaseEstimator, TransformerMixin):
         self.keyword_processor = None
         self.init_flashtext()
 
-    def init_flashtext(self):
+    def init_flashtext(self) -> None:
+        """
+        Initialize the flashtext KeywordProcessor object
+        """
         self.keyword_processor = KeywordProcessor()
 
         for x in self.flashtext_separators:
@@ -50,7 +80,23 @@ class WordLevelTokenizer(BaseEstimator, TransformerMixin):
             )
 
     @staticmethod
-    def _flag_text(flag_dict, text):
+    def _flag_text(flag_dict: Dict[str, str], text: str) -> str:
+        """
+        General flagging: replace remarkable expressions by a flag
+        Ex: 0123456789 => flag_phone_
+
+        Parameters
+        ----------
+        flag_dict: Dict[str, str]
+            Flagging dict with regex as key and replace_text as value
+        text: str
+            Text to be flagged
+
+        Returns
+        -------
+        text: str
+            Flagged text
+        """
         for key, value in flag_dict.items():
             if isinstance(value, dict):
                 text = WordLevelTokenizer._flag_text(value, text)
@@ -59,20 +105,71 @@ class WordLevelTokenizer(BaseEstimator, TransformerMixin):
 
         return text
 
-    def _text_to_tokens(self, text):
+    def _text_to_tokens(self, text: str) -> List[str]:
+        """
+        Split a text into values
+
+        Parameters
+        ----------
+        text: Text to be split
+
+        Returns
+        -------
+        tokens: List[str]
+            List of tokens
+        """
         if isinstance(text, str):
             tokens = re.findall(self.tokenizer_regex, text, re.M + re.DOTALL)
         else:
             tokens = []
         return tokens
 
-    def _remove_stopwords(self, tokens):
+    def _remove_stopwords(self, tokens: List[str]) -> List[str]:
+        """
+        Remove stopwords from tokens.
+
+        Parameters
+        ----------
+        tokens: List[str]
+            List of tokens
+
+        Returns
+        -------
+        tokens: List[str]
+            List of tokens without stopwords
+        """
         return [token for token in tokens if token not in self.stopwords]
 
-    def _flag_names(self, tokens):
+    def _flag_names(self, tokens: List[str]) -> List[str]:
+        """
+        Replace name tokens with a flag
+
+        Parameters
+        ----------
+        tokens: List[str]
+            List of tokens
+
+        Returns
+        -------
+        tokens: List[str]
+            List of tokens with names flagged
+        """
         return [self.keyword_processor.replace_keywords(token) for token in tokens]
 
-    def tokenize(self, text):
+    def tokenize(self, text: str) -> List[str]:
+        """
+        Apply the full tokenization pipeline on a text.
+
+        Parameters
+        ----------
+        text: str
+            Input text to be tokenized
+
+        Returns
+        -------
+        tokens: List[str]
+            List of tokens
+        """
         # Text flagging
         text = self._flag_text(self.flag_dict, text)
 
@@ -87,7 +184,15 @@ class WordLevelTokenizer(BaseEstimator, TransformerMixin):
 
         return tokens
 
-    def save(self, path):
+    def save(self, path: str) -> None:
+        """
+        Save the tokenizer into a json file
+
+        Parameters
+        ----------
+        path: str
+            Save path
+        """
         d = self.__dict__.copy()
         for key in self.EXCLUDE_LIST:
             _ = d.pop(key, None)
@@ -101,7 +206,20 @@ class WordLevelTokenizer(BaseEstimator, TransformerMixin):
             json.dump(d, f)
 
     @classmethod
-    def load(cls, path):
+    def load(cls, path: str):
+        """
+        Load the tokenizer from a json file
+
+        Parameters
+        ----------
+        path: str
+            Load path
+
+        Returns
+        -------
+        _: WordLevelTokenizer
+            Tokenizer instance
+        """
         with open(path, "r") as f:
             params = json.load(f)
 
