555d47acb07b5bf2f0ed3eb0f019dd046044c2e1
Benoit Lebreton <blebreton@quantmetry.com>
2021-09-21 22:58:21 +0200

    MAINT rm comment

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ requirements = [
 exchange_requirements = ["exchangelib>=4.2.0"]
 transformers_requirements = ["transformers==3.4.0"]
 viz_requirements = ["plotly", "streamlit>=0.57.3"]
-tf_probability = ["tensorflow-probability==0.14.0"]  # at MAIF tensorflow-probability==0.11.1
+tf_probability = ["tensorflow-probability==0.14.0"]
 
 # Test dependencies
 setup_requirements = ["pytest-runner"]
