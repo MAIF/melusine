0e69f1ef754836dd914f91f865e28729f1d2bd04
Hugo Perrier <hperrier@quantmetry.com>
2021-12-24 15:39:16 +0100

    Remove old phraser save/load methods

--- melusine/nlp_tools/phraser.py
+++ melusine/nlp_tools/phraser.py
@@ -1,6 +1,5 @@
 import logging
 import gensim
-from melusine.utils.io_utils import get_file_path, save_pkl_generic, load_pkl_generic
 
 logger = logging.getLogger(__name__)
 
@@ -13,72 +12,21 @@ class Phraser:
     FILENAME = "gensim_phraser_meta.pkl"
     PHRASER_FILENAME = "gensim_phraser"
 
-    def __init__(self, input_columns="tokens", output_columns="tokens", **phraser_args):
-        if isinstance(input_columns, str):
-            self.input_columns = (input_columns,)
-        else:
-            self.input_columns = input_columns
+    def __init__(self, input_column="tokens", output_column="tokens", **phraser_args):
 
-        if isinstance(output_columns, str):
-            self.output_columns = (output_columns,)
-        else:
-            self.output_columns = output_columns
-
+        self.input_column = input_column
+        self.output_column = output_column
         self.phraser_args = phraser_args
         self.phraser_ = None
 
     def fit(self, df, y=None):
         """ """
-        input_data = df[self.input_columns[0]]
+        input_data = df[self.input_column]
         phrases = gensim.models.Phrases(input_data, **self.phraser_args)
         self.phraser_ = gensim.models.phrases.Phraser(phrases)
 
         return self
 
     def transform(self, df):
-        df[self.input_columns[0]] = self.phraser_[df[self.output_columns[0]]]
+        df[self.output_column] = self.phraser_[df[self.input_column]]
         return df
-
-    def save(self, path: str, filename_prefix: str = None) -> None:
-        """
-        Save object instance to a pickle file.
-
-        Parameters
-        ----------
-        path: str
-            Save path
-        filename_prefix: str
-            Prefix of the saved object
-        """
-        filepath = get_file_path(self.PHRASER_FILENAME, path, filename_prefix)
-        self.phraser_.save(filepath)
-
-        self.phraser_ = None
-        save_pkl_generic(self, self.FILENAME, path, filename_prefix)
-
-    @classmethod
-    def load(cls, path: str, filename_prefix: str = None):
-        """
-        Load object instance from a pickle file.
-
-        Parameters
-        ----------
-        path: str
-            Load path
-        filename_prefix: str
-            Prefix of the saved object
-
-        Returns
-        -------
-        _: Phraser
-            Phraser instance
-        """
-        # Load Phraser class
-        instance = load_pkl_generic(cls.FILENAME, path, filename_prefix=filename_prefix)
-
-        # Load Gensim Phraser object
-        filepath = get_file_path(cls.PHRASER_FILENAME, path, filename_prefix)
-        phraser = gensim.models.phrases.Phraser.load(filepath)
-        instance.phraser_ = phraser
-
-        return instance
