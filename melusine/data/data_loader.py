ca722ce7ebc3602383f7ee520e5d541cbac92844
Hugo Perrier <hperrier@quantmetry.com>
2021-12-24 11:37:36 +0100

    Update data loading functions

--- melusine/data/data_loader.py
+++ melusine/data/data_loader.py
@@ -2,7 +2,7 @@ import os.path as op
 import pandas as pd
 
 
-def load_email_data():
+def load_email_data(type="raw"):
     """
     Load a csv file containing toy email data
 
@@ -13,6 +13,15 @@ def load_email_data():
     """
 
     data_directory = op.dirname(op.abspath(__file__))
-    email_data_path = op.join(data_directory, "emails.csv")
+    if type == "raw":
+        email_data_path = op.join(data_directory, "emails.csv")
+    elif type == "preprocessed":
+        email_data_path = op.join(data_directory, "emails_preprocessed.csv")
+    elif type == "full":
+        email_data_path = op.join(data_directory, "emails_full.csv")
+    else:
+        raise AttributeError(
+            f"Unknown data type {type}. Choose between 'raw', 'preprocessed' and 'full'"
+        )
 
-    return pd.read_csv(email_data_path, encoding="utf-8", sep=";")
+    return pd.read_csv(email_data_path, encoding="utf-8").fillna("")
