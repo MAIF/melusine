48a7e655ecb67f2c56a44b40f54125c02c40f13d
Tiphaine Fabre <tiphaine.fabre@maif.fr>
2022-04-04 15:30:43 +0200

    fix bumpversion

--- setup.py
+++ setup.py
@@ -113,6 +113,6 @@ setup(
     tests_require=test_requirements,
     extras_require=extras_require,
     url="https://github.com/MAIF/melusine",
-    2.3.3,
+    version='2.3.3',
     zip_safe=False,
 )
