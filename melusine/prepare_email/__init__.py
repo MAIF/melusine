d20c94e02c9b3ce2bdf292d521d0bab51aaafdf7
Hugo Perrier <hugorperrier@gmail.com>
2023-12-12 12:01:18 +0100

    :recycle: Remove legacy prepare emails

--- melusine/prepare_email/__init__.py
+++
@@ -1,8 +0,0 @@
-from .metadata_engineering import MetaExtension, MetaDate, Dummifier, MetaAttachmentType
-
-__all__ = [
-    'Dummifier',
-    'MetaAttachmentType',
-    'MetaDate',
-    'MetaExtension',
-]
